be29fe8c8b95843341785723cd02a35dd3abb560
Matt Soucoup <masoucou@microsoft.com>
2025-02-05 14:52:05 -0800

    Updates to completions and functions

--- 03-CoreGenerativeAITechniques/src/BasicChat-03Ollama/Program.cs
+++ 03-CoreGenerativeAITechniques/src/BasicChat-03Ollama/Program.cs
@@ -3,6 +3,15 @@
 IChatClient client =
     new OllamaChatClient(new Uri("http://localhost:11434/"), "llama3.2-vision");
 
-var response = await client.CompleteAsync("What is AI?");
+// here we're building the prompt
+StringBuilder prompt = new StringBuilder();
+prompt.AppendLine("You will analyze the sentiment of the following product reviews. Each line is its own review. Output the sentiment of each review in a bulleted list and then provide a generate sentiment of all reviews. ");
+prompt.AppendLine("I bought this product and it's amazing. I love it!");
+prompt.AppendLine("This product is terrible. I hate it.");
+prompt.AppendLine("I'm not sure about this product. It's okay.");
+prompt.AppendLine("I found this product based on the other reviews. It worked for a bit, and then it didn't.");
+
+// send the prompt to the model and wait for the text completion
+var response = await client.CompleteAsync(prompt.ToString());
 
 Console.WriteLine(response.Message);
