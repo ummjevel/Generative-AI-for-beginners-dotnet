02478128ad1007a18765ec3648202f73b73e7c71
Bruno Capuano <bcapuano@gmail.com>
2025-07-22 14:52:06 -0400

    Remove Redis output caching and WeatherApiClient from the project

--- 05-AppCreatedWithGenAI/HFMCP.GenImage/HFMCP.GenImage.Web/Program.cs
+++ 05-AppCreatedWithGenAI/HFMCP.GenImage/HFMCP.GenImage.Web/Program.cs
@@ -6,7 +6,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add service defaults & Aspire client integrations.
 builder.AddServiceDefaults();
-builder.AddRedisOutputCache("cache");
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
@@ -20,13 +19,6 @@ builder.Services.AddScoped<IAIConfigurationService, AIConfigurationService>();
 builder.Services.AddScoped<IMCPService, MCPService>();
 builder.Services.AddHttpClient<IAIService, AIService>();
 
-builder.Services.AddHttpClient<WeatherApiClient>(client =>
-    {
-        // This URL uses "https+http://" to indicate HTTPS is preferred over HTTP.
-        // Learn more about service discovery scheme resolution at https://aka.ms/dotnet/sdschemes.
-        client.BaseAddress = new("https+http://apiservice");
-    });
-
 var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
@@ -39,8 +31,6 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAntiforgery();
-
-app.UseOutputCache();
 
 app.MapStaticAssets();
 
